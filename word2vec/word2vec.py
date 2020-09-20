963f0d363b094c3d26b81ef14a0388fe2cd7537f
choi-hyunkyu <69423543+choi-hyunkyu@users.noreply.github.com>
2020-09-20 20:28:17 +0900

    Update word2vec.py

--- word2vec/word2vec.py
+++ word2vec/word2vec.py
@@ -1,20 +1,38 @@
-# gensim 설치 ko.bin 다운로드
-
 from gensim.models import Word2Vec as w2v
 from konlpy.tag import Okt
 import gensim
 import pandas as pd
+import numpy as np
 
 '''
-데이터 불러오기 및 결측치 제거
+데이터 불러오기 및 결측치 확인
 '''
 fixed_data_df = pd.read_csv('./data/original_data.csv')
 fixed_data_df.isnull().sum()
 fixed_data_df.shape
 
+'''
+공백 제거
+'''
+fixed_data_df['document'] = fixed_data_df['document'].str.strip()
+
+'''
+정규 표현식 사용
+'''
+fixed_data_df['document'] = fixed_data_df['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
+fixed_data_df['document'].replace('', np.nan, inplace = True) # 결측치가 아닌 공백을 결측치로 치환
+
+'''
+결측치 제거
+'''
+# fixed_data_df.loc[fixed_data_Df.document.isnull()][:5] # 결측치 출력
 fixed_data_df = fixed_data_df.dropna(axis = 0)
 fixed_data_df.isnull().sum()
 fixed_data_df.shape
+
+'''
+데이터 저장, 198,884개 문장
+'''
 fixed_data_df.to_csv('./data/fixed_data.csv')
 
 '''
@@ -24,10 +42,13 @@ okt = Okt()
 original_data_df = pd.read_csv('./data/fixed_data.csv')
 test_data_df = original_data_df
 
+# 시간 오래 걸림
+stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다'] # 불용어
 tokenized_data = []
 i = 0
 for text in test_data_df['document']:    
-    tokenized_text = okt.morphs(text)
+    tokenized_text = okt.morphs(text, stem = True)
+    tokenized_text = [text for word in tokenized_text if not word in stopwords]
     tokenized_data.append(tokenized_text)
     i += 1
     print('{}/{}'.format(i, len(test_data_df)))
